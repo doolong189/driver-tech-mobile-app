211e6f19d4c912bf7f194c5eed0c52b8f3b81084
Do_Hoang_Long <hoanglong180903@gmail.com>
2025-03-25 08:30:11 +0700

    Coding 25/3/2025

---
+++ app/src/main/java/com/hoanglong180903/driver/api/usecase/UserUseCase.kt
@@ -0,0 +1,9 @@
+package com.hoanglong180903.driver.api.usecase
+
+import retrofit2.Response
+
+class UserUseCase  {
+//    fun getUserInfo(): Response<GetUserInfoResponse> {
+//        return ApiClientManager.getUserInfo()
+//    }
+}
